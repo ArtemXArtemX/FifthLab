0c3a8900ace53890ecb746961e69f95d138a0c48
ArtemXArtemX <tatarnikov.stud@mail.ru>
2024-10-20 19:57:37 +0700

    fifth commit (ResultActivity code)

--- app/src/main/java/com/example/fifthlab/ResultActivity.kt
+++ app/src/main/java/com/example/fifthlab/ResultActivity.kt
@@ -1,20 +1,20 @@
 package com.example.fifthlab
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val textViewResult = findViewById<TextView>(R.id.textViewResult)
+
+        // Получаем переданную сумму
+        val totalPrice = intent.getIntExtra("totalPrice", 0)
+
+        // Отображаем результат
+        textViewResult.text = "Оплатить = $totalPrice рублей"
     }
 }
